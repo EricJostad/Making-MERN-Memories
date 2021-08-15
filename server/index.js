d28d6b9392ef7b7212929150f3a8c66500ac3dd8
EJostad <eric.jostad@outlook.com>
2021-08-14 20:07:48 -0700

    Small change to url string.

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://EricJostad:ProjectTest!.@firstcluster.w8joy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://EricJostad:ProjectTest!.@firstcluster.w8joy.mongodb.net/FirstCluster?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
