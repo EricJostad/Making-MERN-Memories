6cc5b29833172d16b4c91fc5fd4fef35f96c6715
EJostad <eric.jostad@outlook.com>
2021-08-15 10:05:59 -0700

    Added styling to app using @material-ui/core as well as created an imags folder to house all png/jpeg files needed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import memories from './images/memories.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hi, I am your App</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Hello, I am your Memories</Typography>
+                <img src={memories} alt="Memories" height="60"/>
+            </AppBar>
+        </Container>
     );
 }
 
