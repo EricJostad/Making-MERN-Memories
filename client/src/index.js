9e7eb10f8f94d5c9ea330b4045bacbce24f795b0
EJostad <eric.jostad@outlook.com>
2021-08-15 18:52:05 -0700

    Finishing redux setup. Added reducers folder and files.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDom  from 'react-dom';
+import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
@@ -8,6 +8,11 @@ import reducers from './reducers'
 
 import App from './App';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk))); 
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
